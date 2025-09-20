6ca13841138723dd7d3a3e54f5c3621b04aa66d1
Lina Vanessa Salcedo <lvb171@hotmail.com>
2025-09-20 16:18:15 -0500

    feat: Proyecto 1 de Spring Boot
    API REST para registro de usuarios y gestión de notas personales. Incluye autenticación y autorización con JWT.

---
+++ Projects/notes/src/test/java/co/edu/sena/notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.sena.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
